2f2cebf66713a96c1da8201e71d990d71a19c4b7
C-limited <czlczl214@gmail.com>
2025-04-25 00:21:38 +0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://C-limited.github.io/My-Fuwari-Blog",
-  base: "/",
+  site: "https://C-limited.github.io/",
+  base: "/My-Fuwari-Blog/",
   trailingSlash: "always",
   integrations: [
     tailwind(
